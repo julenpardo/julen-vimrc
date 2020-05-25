83ae4a10b995b13cbcc76b297508cde375e45b45
Julen Pardo <julen.pardo@outlook.es>
2020-05-25 21:58:16 +0200

    Replace YCM with Jedi, remove unused stuff

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -15,48 +15,10 @@ sudo apt-get install -y python-dev \
 ################
 # Python modules
 ################
-sudo pip2 install powerline-status \
-                  pylint \
-                  flake8 \
-                  gitlint \
-                  vim-vint \
-                  autopep8
-sudo pip3 install powerline-status \
+pip install powerline-status \
                   pylint \
                   flake8 \
                   gitlint \
                   vim-vint \
                   autopep8
 
-#############
-# PHP modules
-#############
-
-# PHP CodeSniffer
-sudo wget https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar -O /usr/local/bin/phpcs
-sudo chmod a+rx /usr/local/bin/phpcs
-
-# PHPCBF
-sudo wget https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar -O /usr/local/bin/phpcbf
-sudo chmod a+rx /usr/local/bin/phpcbf
-
-# PHP CodeSniffer fixer
-sudo wget http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -O /usr/local/bin/php-cs-fixer
-sudo chmod a+rx /usr/local/bin/php-cs-fixer
-
-
-####################
-# JavaScript modules
-####################
-
-sudo npm install -g eslint \
-                    jshint \
-                    prettier \
-                    jsonlint \
-                    fixjson \
-                    csslint \
-                    htmlhint
-##############
-# Ruby modules
-##############
-sudo gem install mdl
